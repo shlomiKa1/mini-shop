6360dd6a152b805150f21869eddaf35aa202d870
Shlomi <shlomikaufman91@gmail.com>
2026-09-07 22:13:31 +0300

    fix: layout & app files to handle favorites products

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column"}}>
       <Header />
-      <main>
+      <main style={{flex: '1'}}>
         <Outlet />
       </main>
       <Footer />
