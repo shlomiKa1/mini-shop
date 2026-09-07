65b626f2c88e35579e38bf0b4034ada22d882bfd
Shlomi <shlomikaufman91@gmail.com>
2026-09-07 12:23:08 +0300

    feat: add style for footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import "./styles/Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-title">MINI SHOP</p>
+      <p className="footer-text">Online Shop, click and get</p>
+      <p className="creator">&copy; Powered by Momo.k & fakestoreapi</p>
+    </footer>
+  );
+};
+
+export default Footer;
